a8a804767fe9bc618dedac19db9da1c2da7506c4
tesstanya <tessnat@yahoo.com>
2015-04-17 16:03:14 -0700

    Hour 10 added

---
+++ Hour 10/src/DslModem.java
@@ -0,0 +1,11 @@
+public class DslModem extends Modem {
+	/* This class inherits variables and methods from the Modem class
+	by extending the Modem superclass */
+	
+	String method = "DSL phone connection";
+	
+	public void connect() {
+		System.out.println("Connecting to the Internet . . .");
+		System.out.println("Using a " + method);
+	}
+}
---
+++ Hour 10/src/NewRoot.java
@@ -0,0 +1,9 @@
+class NewRoot {
+	public static void main(String[] arguments) {
+		int number = 100;
+		if (arguments.length > 0)
+			number = Integer.parseInt( arguments[0] );
+		System.out.println("The square root of " + 
+			number + " is " + Math.sqrt(number));
+	}
+}
