ce0351ca32885364b31e836b0d19a1fe935877a6
tesstanya <tessnat@yahoo.com>
2015-04-29 12:13:42 -0700

    Hour 10 Activity

---
+++ Hour 10/src/DslModem.java
@@ -0,0 +1,9 @@
+public class DslModem extends Modem {
+	/* This class inherits variables and methods from the Modem class
+	by extending the Modem superclass */
+	
+	//constructor that passes a value to the variable
+	public DslModem() {
+		method = "DSL phone connection";
+	}
+}
